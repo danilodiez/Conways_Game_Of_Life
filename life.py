c9642dd4f833b473972b90c4c781dfeab0669257
danilodiez <danilodiez1@hotmail.com>
2020-09-29 21:52:55 -0300

    Agregado de matriz general

--- life.py
+++ life.py
@@ -5,19 +5,27 @@ import tkinter as tk
 
 root = tk.Tk()
 
+matrizTablero = [[0 for i in range(20)] for j in range(20)]
+print(matrizTablero)
 
+print(matrizTablero[0][0])
+def pintarNegro(i,j):
+    print('casilla',i,j)
+    cuadro = tk.Button(root,bg="black", width=1,height=1,command=lambda: pintarBlanco(i,j)).grid(row=i, column = j)
+    matrizTablero[i][j]=1
+    print(matrizTablero)
 
-def pintarGrilla(i,j, color):
-    print('hola',i,j)
-    cuadro = tk.Button(root,bg=color, width=1,height=1,command=lambda: pintarGrilla(i,j,"white")).grid(row=i, column = j)
+def pintarBlanco(i,j):
+    print('casilla',i,j)
+    cuadro = tk.Button(root,bg="white", width=1,height=1,command=lambda: pintarNegro(i,j)).grid(row=i, column = j)
+    matrizTablero[i][j]=0
+    print(matrizTablero)
 
-def crearBoton():
+def pantallaInicial():
     for i in range(20):
         for j in range(20):
-            cuadro = tk.Button(root,bg="white", width=1,height=1, command=lambda fila=i, col =j: pintarGrilla(fila,col,"black")).grid(row=i, column = j)
-        
-        
+            cuadro = tk.Button(root,bg="white", width=1,height=1, command=lambda fila=i, col =j: pintarNegro(fila,col)).grid(row=i, column = j)
+            
 
-
-crearBoton()
+pantallaInicial()
 root.mainloop()
