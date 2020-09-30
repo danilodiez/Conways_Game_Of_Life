15ad7d15ade5dbc66b1da4cc22587846ec2ee4f6
danilodiez <danilodiez1@hotmail.com>
2020-09-30 10:30:03 -0300

    boton para iniciar la simulacion

--- life.py
+++ life.py
@@ -6,7 +6,6 @@ import tkinter as tk
 root = tk.Tk()
 
 matrizTablero = [[0 for i in range(20)] for j in range(20)]
-print(matrizTablero)
 
 print(matrizTablero[0][0])
 def pintarNegro(i,j):
@@ -21,11 +20,15 @@ def pintarBlanco(i,j):
     matrizTablero[i][j]=0
     print(matrizTablero)
 
+def iniciarSimulacion():
+    print("Simu iniciada")
+    botonInicio = tk.Button(root,text="Iniciar simulacion", width=15, height=2, bg="green", fg="white", command=iniciarSimulacion, state=tk.DISABLED).grid(row=28, column=22) 
+
 def pantallaInicial():
     for i in range(20):
         for j in range(20):
             cuadro = tk.Button(root,bg="white", width=1,height=1, command=lambda fila=i, col =j: pintarNegro(fila,col)).grid(row=i, column = j)
-            
+    botonInicio = tk.Button(root,text="Iniciar simulacion", width=15, height=2, bg="green", fg="white", command=iniciarSimulacion).grid(row=28, column=22)        
 
 pantallaInicial()
 root.mainloop()
